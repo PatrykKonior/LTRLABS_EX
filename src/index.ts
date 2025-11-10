3cb76301b7c4c5088aa27236ce41eb8794735090
Patryk <patrykkonior4@gmail.com>
2025-11-10 21:00:45 +0100

    centralize service exports in src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { calculateItemNetTotal } from './services/calculateItemNetTotal';
+export { calculateItemTotal } from './services/calculateItemTotal';
+export { calculateOrderNetTotal } from './services/calculateOrderNetTotal';
+export { calculateOrderTax } from './services/calculateOrderTax';
